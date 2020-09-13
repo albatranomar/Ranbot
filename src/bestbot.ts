19e325673dacb566828a70956418a12549e38f74
omaralabtran <albatranomar943@gmail.com>
2020-09-13 18:52:11 +0300

    the bot run file added

---
+++ src/bestbot.ts
@@ -0,0 +1,21 @@
+import { Configuration, BestBotClient } from "./library";
+import { PrismaClient } from "@prisma/client";
+
+(global as any).prisma = new PrismaClient();
+(global as any).config = Configuration.getInstance();
+
+import "./library/extensions/Message";
+import "./library/extensions/Member";
+import "./library/classes/general/Formatter";
+
+const bot = new BestBotClient({
+  owners: config.get("bot.owners"),
+  token: config.get("bot.tokens.discord"),
+  builtDirectory: "dist",
+});
+
+(async () => {
+  await prisma.$connect().catch((err) => bot.logger.error(err));
+
+  await bot.start().catch((err) => bot.logger.error(err));
+})();
