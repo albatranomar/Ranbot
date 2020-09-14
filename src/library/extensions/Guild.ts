461a97e2cf20058036e36d68de0605d1c5a8c1a7
omaralabtran <albatranomar943@gmail.com>
2020-09-14 13:02:01 +0300

    create Guild.ts extension

---
+++ src/library/extensions/Guild.ts
@@ -0,0 +1,20 @@
+import { Structures } from "discord.js";
+import { AkairoClient } from "discord-akairo";
+
+export default Structures.extend(
+  "Guild",
+  (Guild) =>
+    class BestBotGuild extends Guild {
+      public get language() {
+        return (this.client as AkairoClient).database.guilds.get(this.id)?.lang ?? config.get("bot.defaultLang") as string
+      }
+
+      public get databaseMembers() {
+        return (this.client as AkairoClient).database.guilds.get(this.id)?.members;
+      }
+
+      public get databaseAliases() {
+        return (this.client as AkairoClient).database.guilds.get(this.id)?.aliases;
+      }
+    }
+);
