d2c7b3452beac34e526dfa6a2140651746b3dbab
omaralabtran <albatranomar943@gmail.com>
2020-09-15 13:51:22 +0300

    update messageInvalid event

--- src/core/events/handler/messageInvalid.ts
+++ src/core/events/handler/messageInvalid.ts
@@ -11,12 +11,14 @@ export default class messageInvalidListener extends Listener {
 
   public async exec(message: Message) {
     // Handle the commands that is has costume aliase from database.
-    for (const Aliase of message.guild.databaseAliases) {
-      let it = Aliase.aliases.find(a => message.content.includes(a));
-      if (it) {
-        message.content = message.content.replace(it, Aliase.commandID);
-        this.client.commandHandler.handle(message);
-        break;
+    if (message.guild.databaseAliases.length > 0) {
+      for (const Aliase of message.guild.databaseAliases) {
+        let it = Aliase.aliases.find(a => message.content.includes(a));
+        if (it) {
+          message.content = message.content.replace(it, Aliase.commandID);
+          this.client.commandHandler.handle(message);
+          break;
+        }
       }
     }
   }
